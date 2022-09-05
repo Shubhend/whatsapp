5cba347b51259c59b55458bb5af4c5ff6887afc3
shubhendu <kumarshubhendu228@gmail.com>
2022-09-05 17:41:27 +0530

    up

--- database/index.js
+++ database/index.js
@@ -1,11 +1,11 @@
 const knex = require('knex')({
     client: 'mysql',
     connection: {
-      host : 'localhost',
-      user : 'root',
-      port:3307,
-      password : '123456',
-      database : 'wpa'
+      host : '66.45.245.146',
+      user : 'voteonl1_1',
+      port:3306,
+      password : 'Shubhendu@12',
+      database : 'wp_test'
     }
   });
 
