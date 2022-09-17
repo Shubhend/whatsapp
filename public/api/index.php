74c7a7bc5daa7a69757d257b908b112683204a1d
shubhendu <kumarshubhendu228@gmail.com>
2022-09-17 19:23:37 +0530

    update

--- public/api/index.php
+++ public/api/index.php
@@ -1,3 +1,4 @@
 <?php
-$url = $_SERVER['SERVER_NAME'].'/whatsapp';
+$protocol = (!empty($_SERVER['HTTPS']) && $_SERVER['HTTPS'] !== 'off' || $_SERVER['SERVER_PORT'] == 443) ? "https://" : "http://";   
+$url = $protocol.$_SERVER['SERVER_NAME'];
 ?>
